affbc8d7c8118003c61dcbf783bbfc8e855fd32b
aimee-liang <aimeeeliang@gmail.com>
2020-11-09 17:21:07 -0500

    Reservation card layout

--- munch-frontend/src/Components/ReservationCard.js
+++ munch-frontend/src/Components/ReservationCard.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 const ReservationCard = () => {
+
+/* something to also work on - ability to delete or edit a reservation */
     return (
         <>
-        <p>Restaurant info here</p>
+        <p>{/* Restaurant Name */}</p>
+        <p>{/* Address */}</p>
+        <p>{/* Date */ }</p>
+        <p>{/* Time */ }</p>
+        <p>{/* Num of guests */ }</p>
         </>
     )
 }
