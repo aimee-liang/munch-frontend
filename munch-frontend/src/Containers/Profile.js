3ee0e98cfc80649da03aace1064ecd5e1276d688
aimee-liang <aimeeeliang@gmail.com>
2020-11-09 17:27:39 -0500

    Working on profile

--- munch-frontend/src/Containers/Profile.js
+++ munch-frontend/src/Containers/Profile.js
@@ -3,6 +3,11 @@ import Header from "../Components/Header"
 import Reservations from "./Reservations"
 
 export default class Profile extends React.Component{
+
+    state={
+        reservations: [] //each user will have an array of reservations, which will then be mapped and cards created
+    }
+
     render(){
         return(
             <>
