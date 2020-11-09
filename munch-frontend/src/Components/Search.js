3c700470cbc653a49a3f23963bd0a92d3e45f807
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-08 23:49:16 -0500

    added neighborhood dropdown to search

--- munch-frontend/src/Components/Search.js
+++ munch-frontend/src/Components/Search.js
@@ -3,12 +3,13 @@ import React from "react"
 class Search extends React.Component{
     state = {
         search: "",
+        location: "lat=40.705138&lon=-74.014096"
         
     }
 
 
     changeHandler = (e) => {
-      //console.log(e)
+      console.log(e)
       this.setState({ [e.target.name]: e.target.value})
   
     }
@@ -16,7 +17,7 @@ class Search extends React.Component{
     searchHandler = (e) => {
       e.preventDefault()
 
-      fetch(`https://developers.zomato.com/api/v2.1/search?q=${this.state.search}&count=20&lat=40.705138&lon=-74.014096&radius=1000&sort=real_distance&order=asc`, {
+      fetch(`https://developers.zomato.com/api/v2.1/search?q=${this.state.search}&count=20&${this.state.location}&radius=1000&sort=real_distance&order=asc`, {
               headers: {
               Accept: "application/json",
               "User-Key": "7dc855cf4405df1034f62de35de0744e"
@@ -42,6 +43,15 @@ class Search extends React.Component{
             <>
             <form className="search" onSubmit={this.searchHandler}>
                 <input type= "text" name="search" placeholder="Search cuisines, specialties, etc." value={this.state.search} onChange={this.changeHandler} />
+                <label>
+                  Neighborhood:
+                  <select name="location" value={this.state.location} onChange={this.changeHandler}>
+                    <option value="lat=40.705138&lon=-74.014096">Flatiron NYC</option>
+                    <option value="lat=40.701085&lon=-73.987546">Flatiron Brooklyn</option>
+                    <option value="lat=40.695914&lon=-73.917302">Bushwick</option>
+                    <option value="lat=40.727006&lon=-73.982990">East Village</option>
+                  </select>
+                </label>
                 <input type="submit" value="Search" />
             </form>
             </>
