ee6e50755c84c6e7d939c50abfb229c38efef068
aimee-liang <aimeeeliang@gmail.com>
2020-11-11 16:21:51 -0500

    Debugging Navbar

--- munch-frontend/src/Components/Navbar.js
+++ munch-frontend/src/Components/Navbar.js
@@ -7,36 +7,36 @@ class Navbar extends React.Component{
     return(
         <>
 
-        <Nav className="justify-content-end" variant="pills" activeKey="/home">
+        <Nav className="justify-content-end" defaultActiveKey="/home">
             
         {this.props.user ? 
         <>
             <Nav.Item>
-                <Nav.Link className="pill" to="/restaurants">Hi, @{this.props.user.username}</Nav.Link>
+                <Nav.Link className="pill" href="/restaurants">Hi, @{this.props.user.username}</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" to="/restaurants"> Search Restaurants</Nav.Link>
+                <Nav.Link className="pill" href="/restaurants">Search Restaurants</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" to="/profile"> View Profile </Nav.Link>
+                <Nav.Link className="pill" href="/profile">View Profile</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" to="/login">
-                    <li onClick={()=> this.props.logMeOut()}> Log Out</li>
+                <Nav.Link className="pill" href="/login">
+                    <li onClick={()=> this.props.logMeOut()}>Log Out</li>
                 </Nav.Link>
             </Nav.Item>
         </>
         :
         <>
             <Nav.Item>
-                <Nav.Link className="pill" to="/signup">Create an Account</Nav.Link>
+                <Nav.Link className="pill" href="/signup">Create an Account</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" to="/login">Login</Nav.Link>
+                <Nav.Link className="pill" href="/login">Login</Nav.Link>
             </Nav.Item>
         </>
         }
