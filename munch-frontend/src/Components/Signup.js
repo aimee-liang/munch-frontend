d6f7b1504a220585e0574558c131cc09cea816c8
aimee-liang <aimeeeliang@gmail.com>
2020-11-11 14:46:32 -0500

    Cleaned up sign up

--- munch-frontend/src/Components/Signup.js
+++ munch-frontend/src/Components/Signup.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import {Form, Button} from "react-bootstrap"
 class Signup extends React.Component{
     state={
         /* avatar: "", // ideally want this to be an option for users to upload an image so it can be more interactive */
@@ -25,21 +25,41 @@ class Signup extends React.Component{
         }))
     }
     
-    // validate = () => {
-    // }
 
 
     render(){
         return(
             <>
-            <h4>Create a new account below</h4>
-            <form onSubmit={this.localSignupHandler}>
+            <h4 className="create-account">Create a new account</h4>
+            <div className="signup">
+
+            <Form className="signup-form" onSubmit={this.localSignupHandler}>
+
                 <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.changeHandler} />
+                <br>
+                </br>
+                <br>
+                </br>
                 <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.changeHandler} />
+                <br>
+                </br>
+                <br>
+                </br>
                 <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.changeHandler} />
+                <br>
+                </br>
+                <br>
+                </br>
                 <input type="password" name="confirm_password" placeholder="Confirm Password" value={this.state.confirm_password} onChange={this.changeHandler} />
-                <input type="submit" value="sign up" />
-            </form>
+                <br>
+                </br>
+                <br>
+                </br>
+                <Button variant="primary" className="signup-button" input type="submit" input="true" value="sign up">Sign up </Button>
+            
+            </Form>
+
+            </div>
             </>
         )
     }
