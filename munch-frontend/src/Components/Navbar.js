1e2fbdb4e1a8fa2369b66be2989a93c747acaf13
aimee-liang <aimeeeliang@gmail.com>
2020-11-11 15:28:27 -0500

    Cleaning up Navbar

--- munch-frontend/src/Components/Navbar.js
+++ munch-frontend/src/Components/Navbar.js
@@ -1,33 +1,51 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import {Nav} from "react-bootstrap"
 
 class Navbar extends React.Component{
     render(){
     return(
         <>
+
+        <Nav className="justify-content-end" activeKey="/home">
+            
         {this.props.user ? 
-        <ul>
-                <li>{this.props.user.username}</li>
-            <NavLink to="/restaurants">
-                <li>Search Restaurants</li> 
-            </NavLink>
-            <NavLink to="/profile">
-                <li>View Profile</li>
-            </NavLink>
-            <NavLink to="/login">
-                <li onClick={()=> this.props.logMeOut()}>Log Out</li>
-            </NavLink>
-        </ul>
+        <>
+            <Nav.Item>
+                <Nav.Link to="/restaurants">Hi, @{this.props.user.username}</Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link to="/restaurants">Search Restaurants</Nav.Link>
+                    {/* <li>Search Restaurants</li>  */}
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link to="/profile">View Profile</Nav.Link>
+                    {/* <li>View Profile</li> */}
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link to="/login">
+                    <li onClick={()=> this.props.logMeOut()}>Log Out</li>
+                </Nav.Link>
+            </Nav.Item>
+        </>
         :
-        <ul>
-            <NavLink to="/signup">
-                <li>Create an Account</li>
-            </NavLink>
-            <NavLink to="/login">
-                <li>Login</li>
-            </NavLink>
-        </ul>
-    }
+        <>
+            <Nav.Item>
+                <Nav.Link to="/signup">Create an Account</Nav.Link>
+                    {/* Create an Account */}
+            </Nav.Item>
+
+            <Nav.Item>
+                <Nav.Link to="/login">Login</Nav.Link>
+                    {/* <li>Login</li> */}
+            </Nav.Item>
+        </>
+        }
+
+    </Nav>
     </>
     )}
 }
