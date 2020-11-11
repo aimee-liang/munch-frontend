23b87af87074807863b3f6488d6bd0441bcedf85
aimee-liang <aimeeeliang@gmail.com>
2020-11-11 10:28:48 -0500

    Pseudocode fetch for reviews

--- munch-frontend/src/Containers/Profile.js
+++ munch-frontend/src/Containers/Profile.js
@@ -12,8 +12,6 @@ export default class Profile extends React.Component{
 
     componentDidMount(){
         const token = localStorage.getItem("token")
-        // Promise.all({
-            // fetch('http://localhost:3000/api/v1/reviews'),
             fetch('http://localhost:3000/api/v1/reservations',{
                 method: 'GET',
                 headers: {
@@ -25,9 +23,27 @@ export default class Profile extends React.Component{
                 reservations: reservationsData
             })))
             .catch(error => console.log("Error", error))
-        // })
     }
-        
+
+    // componentDidMount(){
+    //     const token = localStorage.getItem("token")
+    //     Promise.all([
+    //         fetch('http://localhost:3000/api/v1/reviews'),
+    //         fetch('http://localhost:3000/api/v1/reservations',{
+    //             method: 'GET',
+    //             headers: {
+    //                 Authorization: `Bearer ${token}`,
+    //             }
+    //         })
+    //     ])
+    //         .then(([resp1, resp2]) => Promise.all([resp1.json(), resp2.json()]))
+    //         .then(([reservationsData, reviewsData]) => this.setState(() => ({
+    //             reservations: reservationsData,
+    //             reviews: reviewsData
+    //         })))
+    //         .catch(error => console.log("Error", error))
+    // }
+
     render(){
         return(
             <>
