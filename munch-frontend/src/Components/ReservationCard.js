6d070de22a65cc6aeae202536123b48cf8f2b3f9
aimee-liang <aimeeeliang@gmail.com>
2020-11-10 20:33:40 -0500

    Correcting for time and date

--- munch-frontend/src/Components/ReservationCard.js
+++ munch-frontend/src/Components/ReservationCard.js
@@ -3,14 +3,23 @@ import React from "react"
 const ReservationCard = (props) => {
     /* something to also work on - ability to delete or edit a reservation */
     const reservation = props.reservation
+
+    let isoDate = props.reservation.datetime
+    let dateOnly = new Date(isoDate).toDateString()
+
+    let isoTime = props.reservation.datetime
+    let timeOnly = new Date (isoTime).toLocaleTimeString()
+
+
     return (
         <>
             {/* <p>{console.log(reservation)}</p> */}
         <div key={reservation.id}>
             <h4>{reservation.restaurant.name}</h4>
             <p>{reservation.restaurant.address}</p>
-            <p>Date: {reservation.datetime}</p>
-            <p>Time: {reservation.datetime}</p>
+            {/* <p>Date: {reservation.datetime}</p> */}
+            <p>Date: {dateOnly}</p>
+            <p>Time: {timeOnly}</p>
             <p>Party Size: {reservation.guests}</p>
         </div>
         </>
