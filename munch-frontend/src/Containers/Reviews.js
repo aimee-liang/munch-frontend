97ff754be5658e5a3ab3c96bcb82c8a38eeb893a
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-12 08:06:07 -0500

    commented out unused imports

--- munch-frontend/src/Containers/Reviews.js
+++ munch-frontend/src/Containers/Reviews.js
@@ -1,5 +1,5 @@
 import React from "react"
-import RatingCard from "../Components/RatingCard"
+//import RatingCard from "../Components/RatingCard"
 
 export default class Reviews extends React.Component{
 
