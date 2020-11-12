7a44ce89c8ce1885616660a09b5a323cf859d348
aimee-liang <aimeeeliang@gmail.com>
2020-11-11 21:04:41 -0500

    Included icons on Navbar

--- munch-frontend/src/Components/Navbar.js
+++ munch-frontend/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
+// import { NavLink } from "react-router-dom"
 import {Nav} from "react-bootstrap"
+import {FaSearch, FaPortrait, FaSignOutAlt, FaEllo} from "react-icons/fa"
 
 class Navbar extends React.Component{
     render(){
@@ -12,20 +13,20 @@ class Navbar extends React.Component{
         {this.props.user ? 
         <>
             <Nav.Item>
-                <Nav.Link className="pill" href="/restaurants">Hi, @{this.props.user.username}</Nav.Link>
+                <Nav.Link className="pill" href="/restaurants"><FaEllo/> Hi, @{this.props.user.username}</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" href="/restaurants">Search Restaurants</Nav.Link>
+                <Nav.Link className="pill" href="/restaurants"><FaSearch/> Search Restaurants</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
-                <Nav.Link className="pill" href="/profile">View Profile</Nav.Link>
+                <Nav.Link className="pill" href="/profile"><FaPortrait/> View Profile</Nav.Link>
             </Nav.Item>
 
             <Nav.Item>
                 <Nav.Link className="pill" href="/login">
-                    <li onClick={()=> this.props.logMeOut()}>Log Out</li>
+                    <li onClick={()=> this.props.logMeOut()}><FaSignOutAlt/> Log Out</li>
                 </Nav.Link>
             </Nav.Item>
         </>
