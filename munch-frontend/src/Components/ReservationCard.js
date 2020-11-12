87e60a08177c7045c69039477a6b856178aeab62
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-12 01:44:50 -0500

    tried almost everything to remove a reservation card

--- munch-frontend/src/Components/ReservationCard.js
+++ munch-frontend/src/Components/ReservationCard.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { FaTimesCircle } from "react-icons/fa";
 
 const ReservationCard = (props) => {
     const reservation = props.reservation
@@ -9,6 +10,19 @@ const ReservationCard = (props) => {
     let isoTime = props.reservation.datetime
     let timeOnly = new Date(isoTime).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
 
+    let deleteHandler = () => {
+        const token = localStorage.getItem("token")
+            fetch(`http://localhost:3000/api/v1/reservations/${reservation.id}`,{
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+            .then(resp=> resp.json())
+            .then(deletedReservation => props.deleteReservation(deletedReservation))
+            .catch(error => console.log("Error", error))
+    }
+
 
     return (
         <>
@@ -16,11 +30,14 @@ const ReservationCard = (props) => {
         <div className="resv-container">
 
             <div className="resv-card" key={reservation.id}>
-                <h4 className="resv-rest-name">{reservation.restaurant.name}</h4>
+                <h4 className="resv-rest-name">{reservation.restaurant.name}<FaTimesCircle  className="delete-icon" onClick={deleteHandler}/></h4>
                 <p>{reservation.restaurant.address}</p>
                 <p>Date: {dateOnly}</p>
                 <p>Time: {timeOnly}</p>
                 <p>Party Size: {reservation.guests}</p>
+                
+        
+
             </div>
 
         </div>
--- munch-frontend/src/Containers/Reservations.js
+++ munch-frontend/src/Containers/Reservations.js
@@ -2,14 +2,27 @@ import React from "react"
 import ReservationCard from "../Components/ReservationCard"
 
 class Reservations extends React.Component {
+
+    state={
+        removedReservations: [],
+        remainingReservations: []
+    }
     makeReservationCard = () => {
         const myReservations = this.props.reservations.filter(reservation => reservation.user_id === this.props.user.id)
         const sortedReservations = myReservations.sort((a, b) => new Date(a.datetime) - new Date(b.datetime))
-        console.log(sortedReservations)
-        return sortedReservations.map(reservation => <ReservationCard key={reservation.id} reservation={reservation} /> )
+        let remainingReservations = sortedReservations.filter( ( res ) => !this.state.removedReservations.includes( res ) );
+        //console.log("remaining:", remainingReservations)
+        return remainingReservations.map(reservation => <ReservationCard key={reservation.id} reservation={reservation} deleteReservation={this.deleteReservation}/> )
+        
     }
-
+    
+    deleteReservation = (deletedReservation) => {
+        this.setState({removedReservations: [...this.state.removedReservations, deletedReservation]})
+        
+    }
+    
     render(){
+        //console.log("removed:", this.state.removedReservations)
         return(
             <>
             {this.makeReservationCard()}
